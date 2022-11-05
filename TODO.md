17ccaa9be9d81ef736b870a0ab47b7f5d60f4afd
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-11-05 10:57:33 -0500

    add back to list button

---
+++ TODO.md
@@ -0,0 +1,9 @@
+[] add moviedetail View
+    [x] use redux, [x] use saga
+    
+    [x] add a route for the movie details view
+    [x] display title, image, descripion
+    [] display all genres of movie selected, prob bring this in from database so
+        dispatch -> watcher - > generator -> axios get with params -> result -> redux->DOM
+    [] add back to list button
+    [] remove bad selector from file, use a prop maybe
--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,19 +1,27 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { useEffect } from 'react';
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux'
 
 function MovieDetails() {
     const params = useParams();
-    const movie = useSelector((store)=>store.movies[params.id]);
-    console.log(movie.title);
 
-    useEffect(()=>{
+    //TODO: this needs to change, the params.id -1 could brake id records are deleted in datatable
+    const movie = useSelector((store) => store.movies[params.id - 1]);
+    
 
-    },[params.id]);
+    useEffect(() => {
+
+    }, [params.id]);
 
     return (
         <>
             <h1>{movie.title}</h1>
+            <img src={movie.poster} alt={movie.title} />
+            <p>{movie.description}</p>
+            <Link to={'/'}>
+                <button>Back to List</button>
+            </Link>
+
         </>
     );
 
