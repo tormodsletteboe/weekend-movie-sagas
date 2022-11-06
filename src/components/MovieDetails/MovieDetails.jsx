b01a849b700d7044d8c6468d2442e1a7f47d844b
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-11-05 21:31:23 -0500

    style with ui

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -2,11 +2,20 @@ import { useParams, Link, useHistory } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import Button from '@mui/material/Button';
-import { Card,CardMedia,CardHeader,CardContent,Typography } from '@mui/material';
-
-
+import { Card, CardMedia, CardHeader, CardContent, Typography, CardActions } from '@mui/material';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import './MovieDetails.css';
+
 
 //displays detail view of a selected movie, title, poster image, description and any Genres, also has a back button to the movielist view "homepage"
 function MovieDetails() {
@@ -36,31 +45,56 @@ function MovieDetails() {
         console.log()
     }, [params.id]);
 
+
+
     return (
         <>
-            <Card sx={{ maxWidth: 345 }}>
+            <Card sx={{ maxWidth: 345 }} className='Card'>
                 <CardHeader
                     title={movie.title}
+                    className='CardHeader'
                 />
                 <CardMedia
                     component='img'
                     image={movie.poster}
                     alt={movie.title}
+                    sx={{ maxWidth: 186 }}
+                    className='CardMedia'
                 />
-                {genresForSelectedMovie.map((genre) => (
-                    <Typography key={genre.name}>{genre.name}</Typography>
-                ))}
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                    {movie.description}
-                    </Typography>
+                <Stack direction='row' spacing={1} className='Stack'>
+
+                    {genresForSelectedMovie.map((genre) => (
+                        <Chip label={genre.name} variant="outlined" color="primary" />
+                    ))}
+                </Stack>
+
+                <CardContent className='CardContent'>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                            <Typography>Description...</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                        <Typography variant="body2" gutterBottom>
+                            {movie.description}
+                        </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                    
+                        
                 </CardContent>
-                
-               
+                <CardActions className='CardActions'>
+                    <Button variant='contained' onClick={() => history.push('/')}>Back to List</Button>
+                </CardActions>
+
+
 
             </Card>
 
-            <Button variant='contained' onClick={() => history.push('/')}>Back to List</Button>
+
 
 
         </>
